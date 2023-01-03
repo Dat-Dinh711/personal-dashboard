e95edf6753f9a90c12e807440e88d27062619318
Dat-Dinh711 <datthanh071100@gmail.com>
2023-01-03 09:42:44 +0700

    feat: Wiring up the Add Note Form

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { NoteService } from './../shared/note.service';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from '../shared/note.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-note',
   templateUrl: './add-note.component.html',
-  styleUrls: ['./add-note.component.scss']
+  styleUrls: ['./add-note.component.scss'],
 })
-export class AddNoteComponent {
+export class AddNoteComponent implements OnInit {
+  constructor(private noteService: NoteService, private router: Router) {}
 
+  ngOnInit(): void {}
+
+  onFormSubmit(form: NgForm) {
+    const note = new Note(form.value.title, form.value.content);
+
+    this.noteService.addNote(note);
+    this.router.navigateByUrl('/notes');
+  }
 }
