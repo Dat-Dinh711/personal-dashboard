2c8e430974262da4cbc18a34ca5e217e517b5b52
Dat-Dinh711 <datthanh071100@gmail.com>
2023-01-05 14:47:00 +0700

    feat: Creating the Add Bookmark View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
 import { EditTodoComponent } from './edit-todo/edit-todo.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
@@ -10,6 +11,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 1 } },
+  { path: 'bookmarks/add', component: AddBookmarkComponent },
   { path: 'todos', component: TodosComponent, data: { tab: 2 } },
   { path: 'todos/add', component: AddTodoComponent },
   { path: 'todos/:id', component: EditTodoComponent },
